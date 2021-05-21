3dab81f203f96c2793e6ccdda9300378ddb35ac1
“HanappiZ” <Zenon.Hanappi@gmail.com>
2021-05-21 10:46:00 +0200

    first steps

---
+++ rps-client/src/views/Screen2.vue
@@ -0,0 +1,14 @@
+
+<template>
+    <div>
+        <h1>Screen 2</h1>
+        <send-message screen="2" />
+    </div>
+</template>
+
+<script>
+import SendMessage from '../components/SendMessage.vue'
+export default {
+    components: { SendMessage },
+}
+</script>
---
+++ rps-client/src/views/Screen3.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Screen 3</h1>
+        <send-message screen="3" />
+    </div>
+</template>
+
+<script>
+import SendMessage from '../components/SendMessage.vue'
+export default {
+    components: { SendMessage },
+}
+</script>
